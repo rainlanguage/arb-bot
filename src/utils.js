d892e6fcfa5ea6cad6637d4b20f3103ae258ef9a
rouzwelt <rouzweltt@gmail.com>
2023-10-28 16:14:59 +0000

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -790,7 +790,7 @@ const createViemClient = (chainId, rpcs, useFallbacs = false) => {
                 [...rpcs.map(v => http(v)), ...fallbacks[chainId].transport],
                 { rank: true }
             )
-            : rpcs.map(v => http(v))[0];
+            : fallback(rpcs.map(v => http(v)));
 
     return createPublicClient({
         chain: viemConfig[chainId]?.chain,
