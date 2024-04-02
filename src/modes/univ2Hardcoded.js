c12bef17575bfb4d077adc7c780e6075556612d3
rouzwelt <rouzweltt@gmail.com>
2024-04-02 23:53:32 +0000

    Update univ2Hardcoded.js

--- src/modes/univ2Hardcoded.js
+++ src/modes/univ2Hardcoded.js
@@ -405,7 +405,7 @@ async function checkArb(
         //     toToken.address,
         //     toToken.decimals
         // );
-        const amountOut = undefined
+        const amountOut = undefined;
         if (amountOut !== undefined) {
             console.log(
                 "\x1b[31m%s\x1b[0m",
