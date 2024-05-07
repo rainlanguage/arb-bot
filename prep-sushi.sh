aca61bb9c2afb3ebddab164950f39c7de2447882
rouzwelt <rouzweltt@gmail.com>
2024-05-07 23:28:10 +0000

    Update prep-sushi.sh

--- prep-sushi.sh
+++ prep-sushi.sh
@@ -7,4 +7,3 @@ git submodule update --init --recursive
 (cd lib/sushiswap && nix develop -c pnpm install --frozen-lockfile)
 (cd lib/sushiswap && nix develop -c pnpm exec turbo run build --filter=./packages/sushi -- --outDir ../../../../sushi/dist) 
 (cd lib/sushiswap/packages/sushi && nix develop -c cp package.json ../../../../sushi && cp README.md ../../../../sushi) 
-# (cd sushi/dist && nix develop -c npm pack --pack-destination ../..)
