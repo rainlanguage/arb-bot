cdbcdf236ef27351d9d7cb2cc889054bc646a812
rouzwelt <rouzweltt@gmail.com>
2024-05-07 02:09:40 +0000

    update

--- prep-sushi.sh
+++ prep-sushi.sh
@@ -3,6 +3,7 @@
 set -euxo pipefail
 
 # build and pack the sushi package from sushiswap monorepo submodule
+git submodule update --init --recursive
 (cd lib/sushiswap && nix develop -c pnpm install --frozen-lockfile)
 (cd lib/sushiswap && nix develop -c pnpm exec turbo run build --filter=./packages/sushi) 
-(cd lib/sushiswap/packages/sushi && nix develop -c pnpm pack --pack-destination ../..)
+(cd lib/sushiswap/packages/sushi && nix develop -c pnpm pack --pack-destination ../../../..)
