6a3f87acf457c2b65a2e3866878de169238aaec0
rouzwelt <rouzweltt@gmail.com>
2024-09-22 04:07:17 +0000

    init

--- diag/DiagOrder.sol
+++ diag/DiagOrder.sol
@@ -6,6 +6,8 @@ import {Script} from "../lib/forge-std/src/Script.sol";
 contract DiagOrder is Script {
     function run() external {
         address to = arb_contract_address; // put arb contract address
+        address from = sender_address;
+        vm.startPrank(from);
         bytes memory data = hex"calldata"; // put calldata here without 0x
         (bool success, bytes memory result) = to.call(data);
         (success, result);
